b9235902e4cd7f5a321da7960a8546e1e2d64ca3
Edgar Mercado <emercadogarcia@gmail.com>
2021-11-17 23:08:40 -0400

    configuarcion de postgres

--- sqlinitial.sql
+++ sqlinitial.sql
@@ -4,30 +4,32 @@ CREATE TABLE task (
 	completed boolean DEFAULT false
 );
 
-CREATE TABLE datosxls	{
+CREATE TABLE datosxls	(
   id serial PRIMARY KEY,
-  fecha_alta DATE DEFAULT sysdate,
+  fecha_alta TIMESTAMP,
   fecha	DATE ,
   empresa	VARCHAR(50) NOT NULL,
   trimestre	VARCHAR(25) NOT NULL,
   regsanitario VARCHAR(30) NOT NULL,
   tipoproducto VARCHAR(15) NOT NULL,
   presentacion VARCHAR(50) NOT NULL,
-  factorconv NUMBER,
-  materiaprima NUMBER,
-  clierazonsocial VARCHAR,
-  codigouni VARCHAR,
-  facturadoc VARCHAR,
-  almacensalreg VARCHAR,
-  regionalentrega VARCHAR,
-  saldoanterior NUMBER,
-  importacionprod NUMBER,
-  importmatprima NUMBER,
-  fabricacion NUMBER,
-  devolucion NUMBER,
-  ventas NUMBER,
-  distsucursales NUMBER,
-  bajadestruccion NUMBER,
-  saldofinal NUMBER,
+  factorconv DECIMAL,
+  materiaprima DECIMAL,
+  clierazonsocial VARCHAR(50),
+  codigouni VARCHAR(20),
+  facturadoc VARCHAR(20),
+  almacensalreg VARCHAR(50),
+  regionalentrega VARCHAR(50),
+  saldoanterior DECIMAL,
+  importacionprod DECIMAL,
+  importmatprima DECIMAL,
+  fabricacion DECIMAL,
+  devolucion DECIMAL,
+  ventas DECIMAL,
+  distsucursales DECIMAL,
+  bajadestruccion DECIMAL,
+  saldofinal DECIMAL,
   usuario_alta VARCHAR(20)
-};
+);
+
+
