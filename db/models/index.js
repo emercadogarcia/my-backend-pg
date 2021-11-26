aaaabb3dd63adb9a7bfa755a8c6c34e4cc631ecf
Edgar Mercado <emercadogarcia@gmail.com>
2021-11-26 06:38:33 -0400

    se configuro y creo la tabla customers y con relacion uno a uno. Se prueba el ENDPOINT y funciona correctamente despues de corregir algunos temas.

--- db/models/index.js
+++ db/models/index.js
@@ -1,9 +1,14 @@
-const { User, UserSchema } = require ('./user.model');
 // aqui todos los modelos
+const { User, UserSchema } = require ('./user.model');
+const { Customer, CustomerSchema } = require ('./customer.model');
+
 
 function setupModels(sequelize) {
   User.init(UserSchema, User.config(sequelize));
   // se incremena los modelo cuanto tengan
+  Customer.init(CustomerSchema, Customer.config(sequelize));
+
+  Customer.associate(sequelize.models);
 }
 
 module.exports = setupModels;
