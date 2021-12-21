165e54f9d38cbcc064a1201c2e626ff176126556
Edgar Mercado <emercadogarcia@gmail.com>
2021-12-21 08:22:17 -0400

    limpieza de archivos

--- db/migrations/20211129125321-dbinitial.js
+++ db/migrations/20211129125321-dbinitial.js
@@ -1,10 +1,10 @@
 'use strict';
 //utilizar esta migracion cuando sea ***la primera vez*** las otras quitarlas para que no interfieran.
 
-// const { DataTypes, Sequelize } = require('sequelize');
+const { DataTypes, Sequelize } = require('sequelize');
 
-// const { USER_TABLE, UserSchema } = require('../models/userModel');
-// const { CUSTOMER_TABLE, CustomerSchema } = require('../models/customerModel');
+const { USER_TABLE, UserSchema } = require('../models/userModel');
+const { CUSTOMER_TABLE, CustomerSchema } = require('../models/customerModel');
 // const { CATEGORY_TABLE, CategorySchema } = require('../models/categoryModel');
 // const { PRODUCT_TABLE, ProductSchema } = require('../models/productModel');
 // const { ORDER_TABLE } = require('../models/orderModel');
@@ -12,45 +12,14 @@
 
 module.exports = {
   up: async (queryInterface) => {
-    // await queryInterface.createTable(USER_TABLE, UserSchema);
-    // await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
-    // await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
-    // await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
-    // await queryInterface.createTable(ORDER_TABLE, {
-    //   id: {
-    //     allowNull: false,
-    //     autoIncrement: true,
-    //     primaryKey: true,
-    //     type: DataTypes.INTEGER
-    //   },
-    //   customerId: {
-    //     field: 'customer_id',
-    //     allowNull: false,
-    //     type: DataTypes.INTEGER,
-    //     references: {
-    //       model: CUSTOMER_TABLE,
-    //       key: 'id'
-    //     },
-    //     onUpdate: 'CASCADE',
-    //     onDelete: 'SET NULL'
-    //       },
-    //   createdAt: {
-    //     allowNull: false,
-    //     type: DataTypes.DATE,
-    //     field: 'created_at',
-    //     defaultValue: Sequelize.NOW,
-    //     },
-    // });
-    // await queryInterface.createTable(ORDER_PRODUCT_TABLE, OrderProductSchema);
+    await queryInterface.createTable(USER_TABLE, UserSchema);
+    await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
 
   },
 
   down: async (queryInterface) => {
-    // await queryInterface.dropTable(USER_TABLE);
-    // await queryInterface.dropTable(CUSTOMER_TABLE);
-    // await queryInterface.dropTable(CATEGORY_TABLE);
-    // await queryInterface.dropTable(PRODUCT_TABLE);
-    // await queryInterface.dropTable(ORDER_TABLE);
-    // await queryInterface.dropTable(ORDER_PRODUCT_TABLE);
+    await queryInterface.dropTable(USER_TABLE);
+    await queryInterface.dropTable(CUSTOMER_TABLE);
+
   }
 };
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,5 @@
 const express = require('express');
 
-// const productsRouter = require('./products.router');
-// const categoriesRouter = require('./categories.router');
-// const orderRouter = require('./orders.router');
 const customersRouter = require('./customers.router');
 const usersRouter = require('./users.router');
 const dataXlsRouter = require('./dataxls.router');
@@ -11,9 +8,7 @@ const authRouter = require('./auth.router');
 function routerApi(app) {
   const router = express.Router();
   app.use('/api/v1', router);
-  // router.use('/products', productsRouter);
-  // router.use('/categories', categoriesRouter);
-  // router.use('/orders', orderRouter);
+
   router.use('/customers', customersRouter);
   router.use('/users', usersRouter);
   router.use('/dataload', dataXlsRouter);
